89c838c463f0fb8501c6d354b261cad9830fffbe
adrianpriet <61021661+adrianpriet@users.noreply.github.com>
2022-02-14 06:33:47 -0600

    Update 01-notas.R

--- R/01-notas.R
+++ R/01-notas.R
@@ -1,1 +1,4 @@
-
+## Este fue el codigo que usamos para crear este archivo
+# usethis::use_r("01-notas.R")
+## Es parte de este ejemplo:
+# https://lcolladotor.github.io/rnaseq_LCG-UNAM_2022/introducci%C3%B3n-a-r-y-rstudio.html
